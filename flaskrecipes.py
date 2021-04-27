8dc1bdb182a0fb487b8c8b40ea8d8a3ddcad2c98
Banjac, Igor <igor.banjac@yahoo.com>
2021-04-27 07:30:11 +0200

    login, register

--- flaskrecipes.py
+++ flaskrecipes.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
+from flask_wtf import CSRFProtect
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 
 posts = [
@@ -28,6 +40,28 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # WILL BE REMOVED WITH NEXT PUSH (DB CREATED)
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check username and password.")
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
