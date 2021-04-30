51a61c2f8956c204ec5d56c30d7bb011f5c7dc3c
Banjac, Igor <igor.banjac@yahoo.com>
2021-04-30 09:57:13 +0200

    account page, redirect if has account as next parameter

--- flaskrecipes/routes.py
+++ flaskrecipes/routes.py
@@ -1,8 +1,8 @@
 from flaskrecipes.models import User, Recipe
 from flaskrecipes import app, db, bcrypt
 from flaskrecipes.forms import RegistrationForm, LoginForm
-from flask import render_template, flash, redirect, url_for
-from flask_login import login_user, current_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -62,7 +62,8 @@ def login():
 
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for("home"))
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
         else:
             flash("Login Unsuccessful. Please check email and password.")
     return render_template("login.html", title="Login", form=form)
@@ -72,3 +73,9 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("home"))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Account")
